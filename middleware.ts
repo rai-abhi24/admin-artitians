8a32b202d860270e1819d9874e9471e3174df864
Abhishek Rai <arai62629@gmail.com>
2025-10-01 13:31:14 +0530

    refactor(middleware): update route names and remove debug code

--- middleware.ts
+++ middleware.ts
@@ -4,21 +4,18 @@ import { verifySession } from "@/lib/auth";
 import bcrypt from "bcryptjs";
 
 const APP_ROUTES = [
-    "/leads",
+    "/enquiries",
 ];
 
 export async function middleware(req: NextRequest) {
     const { pathname } = req.nextUrl;
-    const paas = "Artitians@123admin"
-    console.log(await bcrypt.hashSync(paas, 10));
-    
     const isAppRoute = APP_ROUTES.some((p) => pathname.startsWith(p));
     const token = req.cookies.get("session")?.value;
     const isAuth = !!token && (await verifyToken(token));
 
     if (isAuth && pathname === "/") {
         const url = req.nextUrl.clone();
-        url.pathname = "/dashboard";
+        url.pathname = "/enquiries";
         return NextResponse.redirect(url);
     }
 
